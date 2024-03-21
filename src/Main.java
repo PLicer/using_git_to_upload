a490b29b62693412236decead3daa2d5553b806d
hritik.r@ecomexpress.in <hritik.r@ecomexpress.in>
2024-03-21 11:54:35 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+import static java.lang.Math.PI;
+
+interface shape
+{
+    int area();
+
+}
+
+class square implements shape
+{
+    int l;
+    square(int l)
+    {
+        this.l=l;
+    }
+    public int area()
+    {
+        return l*l;
+    }
+}
+
+class circle implements shape{
+
+    int r;
+    circle(int r)
+    {
+        this.r=r;
+    }
+     public int area()
+     {
+         return (int)PI*r*r;
+     }
+}
+
+class areaDiff{
+    public int diffInArea(shape a,shape b)
+    {
+        return a.area()-b.area();
+    }
+}
+
+
+
+public class Main {
+    public static void main(String[] args) {
+      areaDiff diff=new areaDiff();
+        shape a;
+        shape b;
+        System.out.println(diff.diffInArea(a=new square(10),b=new square(8)));
+
+    }
+}
